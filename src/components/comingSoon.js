f8370e7afeb31ddcbe0efe7c2f8dcd8efd5b59a3
mvijil910 <marcelavijil@Marcelas-MacBook-Pro.local>
2024-09-27 15:28:20 -0700

    updating Signature

--- src/components/comingSoon.js
+++ src/components/comingSoon.js
@@ -11,12 +11,12 @@ const ComingSoon = () => {
               For over two centuries, the heart of Jalisco has pulsed to the rhythm of agave harvests, carrying the timeless echoes of tradition. Passed down through generations, a family’s passion and dedication has crafted a tequila that embodies the spirit of their land and the warmth of their heritage. Now, with hearts brimming with pride, we are excited to share this legacy with the world.
               </p>
               <p className="main-text">
-              In October, 2024, we invite you to join us in celebrating life, honoring the bonds of family and community, and savoring the authenticity of our craft. Each sip will take you on a journey—a tribute to sustainability and love, poured into every bottle from the agave fields of Jalisco. This is more than just tequila; it’s a celebration of tradition, connection, and joy that’s meant to be shared.
+              In October 2024, we invite you to join us in celebrating life, honoring the bonds of family and community, and savoring the authenticity of our craft. Each sip will take you on a journey—a tribute to sustainability and love, poured into every bottle from the agave fields of Jalisco. This is more than just tequila; it’s a celebration of tradition, connection, and joy that’s meant to be shared.
               </p>
               <p className="main-text">
               Stay tuned and be part of the story when our treasure is unveiled this October.
               </p>
-              <p className='El Tequila'>Signature</p>
+              <p className='signature'>El Tequila</p>
     </div>
     </Container>
   );
