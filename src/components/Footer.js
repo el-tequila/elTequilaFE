8bb4e4f266af68ff26222160778aef208e511a68
mvijil910 <marcelavijil@Marcelas-MacBook-Pro.local>
2024-10-02 18:47:01 -0700

    committing changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import "./footer-style.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
@@ -9,21 +10,26 @@ const Footer = () => {
         <footer className="footer">
           <div className='inner'>
             <div className="footer-content">
-              <p className="text-left" >
+              <div className='left-content'>
+                <p className="contact-us">Contact Us</p>
+                  <div className='social-media-icons'>
+                    <a href="mailto:info@eltequila.com?subject=Tequila%20Inquiry">
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </a>
+                    <a href="https://www.facebook.com/profile.php?id=61565716603349">
+                      <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                    <a href="https://www.instagram.com/el.tequilaoficial">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="https://x.com/_eltequila_">
+                      <FontAwesomeIcon icon={faXTwitter} />
+                    </a>
+                </div>
+              </div>
+              <p className="text-right" >
                 © Los Alumbrados. All rights reserved. 2024
               </p>
-              <a href="mailto:info@eltequila.com?subject=Tequila%20Inquiry" className="contact-us">Contact Us</a>
-              <div className='social-media-icons'>
-                <a href="https://www.facebook.com/profile.php?id=61565716603349">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="https://www.instagram.com/el.tequilaoficial">
-                  <FontAwesomeIcon icon={faInstagram} />
-                </a>
-                <a href="https://x.com/_eltequila_">
-                  <FontAwesomeIcon icon={faXTwitter} />
-                </a>
-              </div>
             </div>
           </div>
         </footer>
