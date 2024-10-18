1d2ae7f00b94de241e1163ab9c699d19a875b2e5
Veronica Vijil <veronicavijil@yahoo.com>
2024-10-17 17:34:29 -0700

    added google cloud backend url using env variable

--- src/services/tequila.js
+++ src/services/tequila.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 class TequilaDataService {
     addContact(data) {
-        // return axios.post(`${process.env.REACT_APP_API_BASE_URL}http://localhost:5001/api/requests/contacts`, data);
-        return axios.post(`http://localhost:5001/api/requests/contacts`, data);
+        return axios.post(`${process.env.REACT_APP_BE_BASE_URL}/api/requests/contacts`, data);
     }
 }
 export default new TequilaDataService();
