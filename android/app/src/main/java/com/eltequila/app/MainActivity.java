1d3063a3a62b633b1cfa0c3468331a13567fd7f6
mvijil910 <marcelavijil@Marcelas-MBP.lan>
2024-10-24 13:02:32 -0700

    pushing mobile elements

---
+++ android/app/src/main/java/com/eltequila/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eltequila.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ root.render(
   </BrowserRouter>
 );
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Capacitor app is running!');
+  // Your application logic here
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
