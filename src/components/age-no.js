91968583c7d95cf9363703d29c1f3d12c44840c3
mvijil910 <marcelavijil@Marcelas-MBP.lan>
2024-10-24 13:47:10 -0700

    added back button

--- src/components/age-no.js
+++ src/components/age-no.js
@@ -5,12 +5,18 @@ import logo from '../assets/formula1_logo.svg';
 import "./age-verify.css";
 
 const Underage = () => {
+
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate('/');
+  };
   return (
         <div className='age-verify-container'>
             <Container className="age-verify">
             <img src={logo} alt="brand logo" className="brand-age" />
                 <div className="image-container">
                 <p className='age-text'>We're sorry, you must be of legal drinking age to view the contents of this site.</p>
+                  <button className='age-btn' onClick={handleBack}>&lt; BACK</button>
                 </div>
             </Container>
         </div>
